aca07673c736bd9d42b26b540006fdc766b98840
Ismaël Fezzaz <ifezzaz@teradici.com>
2022-03-05 13:30:56 -0800

    add more unit test bencode::parseInteger

--- bencode_parser/test/bencode_parser_test.cpp
+++ bencode_parser/test/bencode_parser_test.cpp
@@ -4,13 +4,38 @@
 #include <gmock/gmock.h>
 #include <variant>
 
-TEST(BencodeParser, parseIntegerZero)
+class BencodeParserTest : public testing::Test
 {
-    std::stringstream ss;
-    ss.str("i0e");
+protected:
+    std::stringstream _ss;
+};
 
-    auto bvalue = bencode::parseInteger(ss);
+TEST_F(BencodeParserTest, parseIntegerZero)
+{
+    _ss.str("i0e");
+
+    auto bvalue = bencode::parseInteger(_ss);
 
     ASSERT_THAT(std::holds_alternative<bencode::BencodeInt>(bvalue.value), testing::IsTrue());
     EXPECT_THAT(std::get<bencode::BencodeInt>(bvalue.value), testing::Eq(0));
+}
+
+TEST_F(BencodeParserTest, parseIntegerPositiveNumber)
+{
+    _ss.str("i42e");
+
+    auto bvalue = bencode::parseInteger(_ss);
+
+    ASSERT_THAT(std::holds_alternative<bencode::BencodeInt>(bvalue.value), testing::IsTrue());
+    EXPECT_THAT(std::get<bencode::BencodeInt>(bvalue.value), testing::Eq(42));
+}
+
+TEST_F(BencodeParserTest, parseIntegerNegativeNumber)
+{
+    _ss.str("i-21e");
+
+    auto bvalue = bencode::parseInteger(_ss);
+
+    ASSERT_THAT(std::holds_alternative<bencode::BencodeInt>(bvalue.value), testing::IsTrue());
+    EXPECT_THAT(std::get<bencode::BencodeInt>(bvalue.value), testing::Eq(-21));
 }
